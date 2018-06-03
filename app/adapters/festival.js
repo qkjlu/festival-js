11e00743a2c5a4b656d143390b80a374cf0b5199
Lucas <lucas_mas@msn.com>
2018-06-03 02:07:54 +0200

    modif host adapter for deploy

--- app/adapters/festival.js
+++ app/adapters/festival.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-	host: 'https://enigmatic-mesa-54192.herokuapp.com/',
+	host: 'https://enigmatic-mesa-54192.herokuapp.com',
 	namespace : 'api/v1',
 });
--- app/adapters/ville.js
+++ app/adapters/ville.js
@@ -1,6 +1,6 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-	host: 'https://enigmatic-mesa-54192.herokuapp.com/',
+	host: 'https://enigmatic-mesa-54192.herokuapp.com',
 	namespace : 'api/v1',
 });
