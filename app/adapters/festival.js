fb2e67381f3b605baa6ecf0fc17a9e06628b7eb9
Lucas <lucas_mas@msn.com>
2018-06-03 02:03:58 +0200

    commit

--- app/adapters/festival.js
+++ app/adapters/festival.js
@@ -1,6 +1,6 @@
 import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
-	host: 'http://localhost:3000',
+	host: 'https://enigmatic-mesa-54192.herokuapp.com/',
 	namespace : 'api/v1',
 });
--- app/adapters/ville.js
+++ app/adapters/ville.js
@@ -1,6 +1,6 @@
 import ApplicationAdapter from './application';
 
 export default ApplicationAdapter.extend({
-	host: 'http://localhost:3000',
+	host: 'https://enigmatic-mesa-54192.herokuapp.com/',
 	namespace : 'api/v1',
 });
